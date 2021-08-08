d0fab0d703d58a9a45d3996eadfd21697ceaf611
Reagan Chisholm <reaganchisholm@gmail.com>
2021-08-08 14:07:45 -0400

    Fill

--- chapter-04/04/fill/Fill.asm
+++ chapter-04/04/fill/Fill.asm
@@ -11,4 +11,68 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(KEYCHECK)
+    @KBD
+    D=M
+
+    // Draw screen black if keyboard value is > 0
+    @BLACK
+    D;JGT
+
+    // Otherwise, we clear the screen to white
+    @WHITE
+    0;JMP
+
+(BLACK)
+    // set our draw value to -1 (1111111111111111)
+    @R0
+    M=-1
+    @DRAW
+    0;JMP
+
+(WHITE)
+    // set our draw value to 0 (0000000000000000)
+    @R0
+    M=0
+    @DRAW
+    0;JMP
+
+(DRAW)
+    // Set the counter (R1) to 8192 (this is how many blits we need to do).
+    // We will keep counting down with this as we draw.
+    @8191
+    D=A
+    @R1
+    M=D
+
+    // Walk the screen and set the values to R0 (the value we set above).
+    (NEXT)
+        // Calculate the position.
+        @R1
+        D=M
+        @pos
+        M=D
+        @SCREEN
+        D=A
+        @pos
+        M=M+D
+
+        // Actually draw the value at the current position.
+        @R0
+        D=M
+        @pos
+        A=M
+        M=D
+
+        // Decrement the counter (R1).
+        @R1
+        D=M-1
+        M=D
+
+        // Next if the counter is still >= 0.
+        @NEXT
+        D;JGE
+
+    // Loop back around.
+    @KEYCHECK
+    0;JMP
