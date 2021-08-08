4c32bbe249060e914bd9fa7161fb15e702b0d09c
Reagan Chisholm <reaganchisholm@gmail.com>
2021-08-08 13:20:34 -0400

    Mult

--- chapter-04/04/mult/Mult.asm
+++ chapter-04/04/mult/Mult.asm
@@ -9,4 +9,41 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// set answer to 0 at start
+@R2	
+M=0 	
+
+// If R0 is 0, answer will be 0, so we jump to end
+@R0
+D=M
+@END
+D;JEQ
+
+// If R1 is 0, answer will be 0, so we jump to end
+@R1
+D=M
+@END
+D;JEQ
+
+// use for multiply count
+@R0
+D=M
+@R3 
+M=D
+
+// loop for adding
+(LOOP)
+    @R1
+    D=M
+    @R2 // sum
+    M=M+D // sum = sum + R1
+
+    @R3	
+    M=M-1 // take 1 off multiply count
+    D=M
+    @LOOP
+    D;JGT // if multiple count (R3) > 0 we loop back and keep adding
+
+(END)
+    @END
+    0;JMP // infinite loop
